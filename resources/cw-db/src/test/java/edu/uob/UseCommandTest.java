ceca04e6fd33bc0b186f4989c89cb069543d9dc4
ChenPin-Ru <nezii0331@gmail.com>
2025-03-11 14:18:25 +0000

    give little thing

--- resources/cw-db/src/test/java/edu/uob/UseCommandTest.java
+++ resources/cw-db/src/test/java/edu/uob/UseCommandTest.java
@@ -2,18 +2,19 @@ package test.java.edu.uob;
 import org.junit.jupiter.api.Test;
 
 public class UseCommandTest {
-//    @Test
-//    void testParseCommand() {
-//        UseCommandParser parser = new UseCommandParser();
-//        assertTrue(parser.parseCommand("USE markbook;"));
-//        assertEquals("markbook", parser.getDatabaseName());
-//    }
-//
-//    @Test
-//    void testInvalidCommand() {
-//        UseCommandParser parser = new UseCommandParser();
-//        assertFalse(parser.parseCommand("USE;")); // This should return false
-//    }
+    @Test
+    void testParseCommand() {
+        UseCommandParser parser = new UseCommandParser();
+        assertTrue(parser.parseCommand("USE markbook;"));
+        assertEquals("markbook", parser.getDatabaseName());
+    }
+
+    @Test
+    void testInvalidCommand() {
+        UseCommandParser parser = new UseCommandParser();
+        assertFalse(parser.parseCommand("USE;")); // This should return false
+    }
+
 
     @Test
     void testCreateDatabase() {
